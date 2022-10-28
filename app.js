246e8a5e73faa0ab74cde36c15216a062da3069b
erin <erin.gwc@gmail.com>
2022-10-28 01:39:14 -0700

    Build generateRecord() and compareNumbers()

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ gameOverMessage.classList.add("hide");
 const wrapper = document.querySelector(".wrapper");
 const restartButton = document.querySelector(".restart");
 const restartButtonTwo = document.getElementById("gameOverRestart");
+const playDashboard = document.getElementById("playDashboard");
 const guessedNumber = document.querySelector(".guessedNumber");
 const result = document.querySelector(".result");
 const upDown = document.querySelector(".upDown");
-const myNumber = myRandomNumber();
+const myNumber = generateMyNumber();
+let newRow; //= document.createElement("tr");
+let arrowResponse; //= document.createElement("th");
+let userInputNumber; // = document.createElement("th");
+
+// const h3 = document.querySelector("h3");
+// h3.classList.add("hide");
 let maxAttempt = 5; 
-let winGame = false;
+let winner = false;
 
 form.addEventListener("submit", startGame);
 hint.addEventListener("click", generateHint);
@@ -19,140 +26,103 @@ restartButton.addEventListener("click", restartGame);
 restartButtonTwo.addEventListener("click", restartGame);
 
 function restartGame() {
-    alert("reloaded");
     document.location.reload(true);
 }
-
-function myRandomNumber() {
+function generateMyNumber() {
     const randomNum = Math.floor(Math.random() * 100) + 1; 
     return randomNum;
 }
-
 function getGameOverMessage() {
     input.value = "";
     wrapper.classList.add("hide");
     gameOverMessage.classList.remove("hide");
 }
-
 function startGame() {
     event.preventDefault();
     const inputNumber = Number(userInput.value);    
     if(maxAttempt > 0) {
         input.value = "";
-        alert(maxAttempt);
-        compareNumbers(inputNumber);    
+        // generateRecordSign();
         maxAttempt--;
-        // alert(maxAttempt);
-    }
-    if (maxAttempt === 0 && winGame == false) {
-        // alert("OVER!" + maxAttempt);    
-        getGameOverMessage();
+        compareNumbers(inputNumber);    
+    } 
+    if (maxAttempt === 0 && winner == false) {  
+        input.disabled = true; 
+        setTimeout(getGameOverMessage, 2000);
     }
 }
+function resetMaxAttempt() {
+    maxAttempt = 5;
+}
+function winGame() {
+    winner = true;
+    addTrophy();
+    resetMaxAttempt(); // this works
+    setTimeout(clearRecord, 2000);
+    startGame;
+}
+// function generateRecordSign() {
+//     h3.classList.remove("hide");
+// }
+function clearRecord() {
+    guessedNumber.removeChild(newRow);
+    newRow.removeChild(arrowResponse);
+    newRow.removeChild(userInputNumber);
+}
 
-function compareNumbers(inputNumber) {
-    // alert(inputNumber + " " + myNumber)
-    if(inputNumber === myNumber) { 
-        // winGame();    
+function generateRecord(inputNumber) {
+    newRow = document.createElement("tr");
+    arrowResponse = document.createElement("th");
+    userInputNumber = document.createElement("th");
     
-    } else if (inputNumber > myNumber) {
-        
+    guessedNumber.appendChild(newRow);
+    userInputNumber.textContent = inputNumber;
+    if(inputNumber > myNumber) {
+        arrowResponse.textContent = "LOWER ↓";
+        arrowResponse.style.color = "#f93947";
+        newRow.appendChild(userInputNumber);
+        newRow.appendChild(arrowResponse);  
     } else if (inputNumber < myNumber) {
-        
+        arrowResponse.textContent = "HIGHER ↑";
+        // userInputNumber.style.color = "#00887A";
+        arrowResponse.style.color = "#00887A";
+        newRow.appendChild(userInputNumber);
+        newRow.appendChild(arrowResponse);
+    } else {
+        arrowResponse.textContent = " 🏆 ";
+        userInputNumber.style.color = "#00887A";
+        newRow.style.backgroundColor = "rgba(0, 136, 122, 0.2)";
+        newRow.style.borderRadius = "25px";
+        newRow.appendChild(userInputNumber);
+        newRow.appendChild(arrowResponse);
+    }
+}
+function compareNumbers(inputNumber) {
+    // generateRecordSign();
+    if(inputNumber === myNumber) { 
+        generateRecord(inputNumber);
+        winGame();    
+    } else if (inputNumber > myNumber) {
+        generateRecord(inputNumber);
+    } else if (inputNumber < myNumber) {
+        generateRecord(inputNumber);
     }
 }
 
-// if(maxAttempt < 0) {
-//     form.addEventListener("submit", (event) => {
-//         event.preventDefault();
-//         startGame();
-//         maxAttempt--; // this works)
-//     });
-// } else {
-//     // alert("OVER");  => This doesn't seem to be working.. is it because of preventDefault??
-// }
-
-//  // table > tr > td(trophy)
-//  const trophyTest = document.createElement("td");
-//  // trophyTest.style.border = "3px solid orange";
-//  const trophyBox = document.querySelector("tr");
-//  const trophy = document.createElement("img");
-//  //######## BUG: in mobile screen, box(row) increased...#######
-//  trophy.style.width = '2rem';
-//  trophy.style.height = '70px';
-//  trophy.src = "./images/trophyImage.png";
-//  trophyBox.appendChild(trophy);
-//  table.appendChild(trophyBox);
-
-
-
-
-
-// let form = document.querySelector("form");
-// let input = document.querySelector("input");
-// let result = document.querySelector("#result");
-// form.addEventListener("submit", (event) => {
-//     event.preventDefault(); 
-//     let number = input.value;
-//     let translate = fizzbuzz(number);
-//     result.textContent = translate;
-//     input.value = "";
-// });
-
-// EXAMPLE to call eventListener...
-// let input = document.querySelector("input");
-// let result = document.querySelector("#result");
-// form.addEventListener("submit", (event) => {
-//     event.preventDefault(); 
-//     let number = input.value;
-//     let translate = fizzbuzz(number);
-//     result.textContent = translate;
-//     input.value = "";
-// });
-
-
-
-// 2. create a function to compare input number to target! 
-//  for(int i = 0; i<5; i++) {
-//     if(target == inputNumber[i]) => Congratulations! with addTrophy and restart function; 
-//         else if (target > inputNumber[i]) {
-//             up arrow 
-//         } else if (target < inputNumber[i]) {
-//             down arrow 
-//         }
-//  }
-// main() {
-//     generate target number;     
-// }
-
-
-
-//function for down  with color in the result div  
-//function for up  with color in the result div
-//function to restart game... when do you reset trophy box?? 
-
-//Need to give 10 hints away! 
-// 1. generate 10 numbers except numbers use entered
-
-//add trophy to trophy box 
 function addTrophy() {
-    // table > tr > td(trophy)
-    const trophyTest = document.createElement("td");
-    // trophyTest.style.border = "3px solid orange";
-    const trophyBox = document.querySelector("tr");
+    const trophyContainer = document.createElement("div");
     const trophy = document.createElement("img");
-    //######## BUG: in mobile screen, box(row) increased...#######
-    trophy.style.width = '2rem';
-    trophy.style.height = '70px';
-    trophy.src = "./images/trophyImage.png";
-    trophyBox.appendChild(trophy);
-    table.appendChild(trophyBox);
-    //####### ADD not to refresh each session unless user reloads page
+    trophy.src = "./images/trophyImage.png"
+    trophyBox.appendChild(trophyContainer);
+    trophyContainer.appendChild(trophy);
+    trophyContainer.style.width = "40px";
+    trophy.style.width = "40px";
+    trophyContainer.style.height = "50px";
+    trophy.style.height = "50px";
 }
 function generateHint() {
     let count = 0; 
     const hintArray = [];
-    // alert(hintArray + "target: " + myNumber);
     while(count<10) {
         const randomNumber = Math.floor(Math.random() * 100) + 1;
         if(!(hintArray.includes(randomNumber))) {
@@ -166,3 +136,11 @@ function generateHint() {
     hintList.textContent = "HINT: " + displayHintArray;
     input.value = ""; //is it needed?
 }
+
+// NOTE
+// 1. restart game during the game should reload the whole page 
+// 2. But after winning a game should just give maxattempt back to 5
+// 3. can't make it small trophies for small screens 
+
+
+
